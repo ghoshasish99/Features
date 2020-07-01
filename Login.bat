3d47e0cd66583aba8dfd5df4dc4d0ee1a06179c0
Ashish Ghosh <ghoshasish99@gmail.com>
2020-07-02 00:09:15 +0200

    Initial commit

---
+++ Login.bat
@@ -0,0 +1,9 @@
+echo Feature: Login>>Login.feature
+
+echo   I want to login to the ParaBank>>Login.feature
+  
+echo   Scenario: Login to ParaBank>>Login.feature
+echo     Given I open ParaBank>>Login.feature
+echo     When I enter UserName and Password>>Login.feature
+echo     And I click on Submit>>Login.feature
+echo    # Then I should see my Dashboard>>Login.feature
