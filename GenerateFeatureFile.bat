6dc0e9ec577f195a71e585404cacca74e9e7102b
Ashish Ghosh <ghoshasish99@gmail.com>
2020-07-06 21:35:37 +0200

    JIRA-41720

--- GenerateFeatureFile.bat
+++ GenerateFeatureFile.bat
@@ -6,4 +6,17 @@ echo   Scenario: Login to ParaBank>>Login.feature
 echo     Given I open ParaBank>>Login.feature
 echo     When I enter UserName and Password>>Login.feature
 echo     And I click on Submit>>Login.feature
-echo    # Then I should see my Dashboard>>Login.feature
+echo     Then I should see my Dashboard>>Login.feature
+
+echo   Scenario: Get User Details via API>>Login.feature
+echo     When I request User Details>>Login.feature
+echo     Then I should receive them>>Login.feature
+
+
+echo Feature:  Registration>>Register.feature
+
+echo    Scenario: Register to Parabank>>Register.feature
+echo      Given I open Registration Link>>Register.feature
+echo      When I fill in registration details>>Register.feature
+echo      And I click on Register Button>>Register.feature
+echo      Then I should see Welcome message>>Register.feature
